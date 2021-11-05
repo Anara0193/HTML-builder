71d4c6582d7cf5b453f850320ae34b0674fd0f60
AbubakritoUmarito1718 <zhaksybaeva30@gmail.com>
2021-11-05 20:53:25 +0600

    feat: write file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,16 +1,31 @@
 const fs = require('fs');
 const readline = require('readline');
+const process = require('process');
 const rl = readline.createInterface({
   input : process.stdin,
   output : process.stdout
 });
 
-rl.question(`Enter text`,
-(userInput)=>{
-  console.log(userInput);
-  fs.writeFile("02-write-file/hello.txt", userInput, function(error){
-    if(error) throw error; 
-  });
+function fileHandler(){
+  fs.open('02-write-file/text.txt', 'w', (err) => {
+      if(err) throw err;
+      console.log('Enter text');
+  });  
+}
+
+fileHandler();
+
+rl.on('line', (input) => {
+  if(input === 'exit') {
+    console.log('Input completed');
+    process.exit();
+  }
+  fs.appendFile('02-write-file/text.txt', `${input} \n`, (err) => {
+    if(err) throw err;
+});
 });
 
-
+rl.on('SIGINT', () => {
+  rl.close();
+  console.log('Input completed');
+});
